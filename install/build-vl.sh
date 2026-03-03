76341679aceb5afd64659c79aa7a9fbfdf8e3199
Bruno Cornec <bruno@victoria.frmug.org>
2026-03-04 00:49:15 +0100

    Launch VM if necessary before vl up

--- install/build-vl.sh
+++ install/build-vl.sh
@@ -7,6 +7,14 @@ EXEPATH=`dirname "$0"`
 #
 # Prepare the conf file for vl
 $EXEPATH/build-vl.pl
+# Start the VM if necessary
+for v in `grep name: virt-lightning.yaml | cut -d: -f2`; do
+	LANG=C virsh -c qemu:///system list | grep $v | grep -q 'shut off'
+	if [ $? -eq 0 ]; then
+		virsh -c qemu:///system start $v
+		sleep 30
+	fi
+done
 # Ask vl to launch the VMs
 vl up
 # Install the WoD stack
