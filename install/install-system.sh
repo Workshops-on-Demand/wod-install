74c13c40a451344c4ae3f6d9fd8608a9af8e2e13
Bruno Cornec <bruno@victoria.frmug.org>
2026-05-27 10:32:36 +0200

    Use WODAPIDBEXTPORT for listening

--- install/install-system.sh
+++ install/install-system.sh
@@ -431,7 +431,7 @@ EOF
     configure_pm2 $WODAPIDBDIR
     # If reverse proxy was asked perform additional tasks
     if [ _"$WODREVPROXY" != _"0" ]; then
-        sudo certbot --nginx -d $WODAPIDBEXTFQDN,$WODFEEXTFQDN,$WODBEEXTFQDN -n --agree-tos -m $WODREVPROXY
+        sudo certbot --nginx -d $WODAPIDBEXTFQDN,$WODFEEXTFQDN,$WODBEEXTFQDN -n --https-port $WODAPIDBEXTPORT --agree-tos -m $WODREVPROXY
     fi
 elif [ $WODTYPE = "frontend" ]; then
     cd $WODFEDIR
